6c0daff3290f810ca29065c18d56e3515a28a046
Amy Zhang <147532038+amysfzhang@users.noreply.github.com>
2024-03-06 17:00:59 -0500

    Create ClimberSubsystem.java
    Not sure what you meant for speed and type to be final

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.GenericController;
+import frc.robot.util.GenericController.BaseController;
+import frc.robot.util.GenericController.NeutralMode;
+
+public class ClimberSubsystem {
+
+    public static class Climber extends SubsystemBase {
+
+        private GenericController motor;
+
+        private Climber(
+            BaseController type,
+            int motorID,
+            boolean invert
+        ) {
+            motor = new GenericController(type, mID);
+            motor.setInverted(invert);
+        }
+        
+        public static Climber create(
+            BaseController type, 
+            int mID, 
+            boolean invert
+        ) {
+            return new Climber(type, int motorID, boolean invert);
+        }
+ 
+        public void setMotor(double speed) {
+            motor.set(speed);
+        }
+
+        public void setNeutralMode(neutralMode) {
+            motor.setNeutralMode(neutralMode);
+        }
+       
+    }
+}
