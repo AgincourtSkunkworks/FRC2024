4d336e1e6bcc2f995f418b3c70edcaff21939659
Theodore Hua <contact@theodorehua.dev>
2024-03-07 13:06:22 -0500

    Reformat

--- src/main/java/frc/robot/commands/ClimberPID.java
+++ src/main/java/frc/robot/commands/ClimberPID.java
@@ -4,31 +4,32 @@ import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.util.DynamicValue;
 
 public class ClimberPID extends GenericPID {
+
     DynamicValue<Double> tolerance;
-    
+
     public ClimberPID(
-            String name,
-            ClimberSubsystem climber,
-            double target,
-            DynamicValue<Double> targetTolerance,
-            double defaultP,
-            double defaultI,
-            double defaultD,
-            double defaultIMax
+        String name,
+        ClimberSubsystem climber,
+        double target,
+        DynamicValue<Double> targetTolerance,
+        double defaultP,
+        double defaultI,
+        double defaultD,
+        double defaultIMax
     ) {
         super(
-                name,
-                defaultP,
-                defaultI,
-                defaultD,
-                defaultIMax,
-                () -> target - climber.getPosition(),
-                climber::setMotor
+            name,
+            defaultP,
+            defaultI,
+            defaultD,
+            defaultIMax,
+            () -> target - climber.getPosition(),
+            climber::setMotor
         );
         addRequirements(climber);
         tolerance = targetTolerance;
     }
-    
+
     @Override
     public boolean isFinished() {
         return Math.abs(getError.getAsDouble()) < tolerance.get();
